0693741c1dd32260a368b8dbe8e644562d1d9761
fescii <isfescii@gmail.com>
2024-04-14 00:30:45 +0300

    Story entities; Entry A12

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,7 +2,7 @@ const { authController } = require('../controllers');
 const { authMiddleware } = require('../middlewares');
 
 module.exports = (app, url) => {
-  app.use((req, res, next) => {
+  app.use((_req, res, next) => {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
