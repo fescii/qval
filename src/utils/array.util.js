be73b5d723fdc6676441d27c1b634c8a8a1acfa6
fescii <isfescii@gmail.com>
2024-04-13 23:17:11 +0300

    Story entities; Entry A09

--- src/utils/array.util.js
+++ src/utils/array.util.js
@@ -1,11 +1,25 @@
-mapFields = (arr, field) => {
+// Mapping array of objects to array of fields
+const mapFields = (arr, field) => {
   return arr.map(item => item[field])
 }
-sumArray = (arr) => arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+// Summing array of numbers
+const sumArray = (arr) => arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+
+// Mapping array of string to slug fields using regex
+
+const slugify = (text) => {
+  return text.trim().replace(/\s+/g, ' ').
+    toLowerCase().replace(/\s+/g, '-');
+}
+
+const slugifyArray = (arr) => {
+  return arr.map(item => slugify(item));
+};
 
 const arrayUtil = {
-  mapFields: mapFields,
-  sumArray: sumArray
+  mapFields, sumArray, slugifyArray, slugify
 };
 
 module.exports = arrayUtil;
--- src/validators/story.validator.js
+++ src/validators/story.validator.js
@@ -1,4 +1,5 @@
-const { sanitizeUtil } = require('../utils')
+const { sanitizeUtil } = require('../utils');
+const { slugifyArray, slugify } = require('../utils').arrayUtil;
 
 // Register data validation
 const validateStoryData = async (data) => {
@@ -9,87 +10,78 @@ const validateStoryData = async (data) => {
     }
   }
 
-  // Check if the story is of kind === post
-  if (data.kind === 'post') {
-    if (data.content && data.topics) {
-      // validate first name
-      if (typeof data.content !== 'string' || data.content.length < 2) {
-        return {
-          data: null,
-          error: new Error("Post content should have 2 chars or more and must be a string!")
-        }
-      }
-
-      // validate last name
-      if (typeof data.topics !== 'string' || data.topics.length < 2) {
-        return {
-          data: null,
-          error: new Error("Post topics should have 2 chars or more and must be a string!")
-        }
-      }
-
-      const validatedData = {
-        content: await sanitizeUtil.sanitizeInput(data.content),
-        topics: await sanitizeUtil.sanitizeInput(data.topics),
-        kind: data.kind
-      }
-
-      return { data: validatedData, error: null };
-    }
-    else {
-      return {
-        data: null,
-        error: new Error("Some fields were not provided or contains null values, Ensure you provide: (content, topics)")
-      }
+  // validate content
+  if (typeof data.content !== 'string' || data.content.length < 2) {
+    return {
+      data: null,
+      error: new Error("Content should be a string, should be at least 2 chars long!")
     }
   }
 
-  if (data.kind === 'story' || data.kind === 'article' || data.kind === 'story') {
-    if (data.title && data.body && data.topic) {
-      // validate first name
-      if (typeof data.title !== 'string' || data.title.length < 2) {
-        return {
-          data: null,
-          error: new Error("Story title should have 2 chars or more and must be a string!")
-        }
-      }
-
-      // validate last name
-      if (typeof data.body !== 'string' || data.body < 30) {
-        return {
-          data: null,
-          error: new Error("Story body should have 30 chars or more and must be a string!")
-        }
-      }
-
-      // validate last name
-      if (typeof data.topic !== 'string' || data.topic < 2) {
-        return {
-          data: null,
-          error: new Error("Story topic should have 2 chars or more and must be a string!")
-        }
-      }
-
-      const slug_data = await sanitizeUtil.sanitizeInput(data.title);
-
-      const validatedData = {
-        title: await sanitizeUtil.sanitizeInput(data.title),
-        body: await sanitizeUtil.sanitizeInput(data.body),
-        topic: await sanitizeUtil.sanitizeInput(data.topic),
-        kind: data.kind,
-        slug: slug_data.trim().replace(/\s+/g, ' ').
-          toLowerCase().replace(/\s+/g, '-'),
-      }
-
-      return { data: validatedData, error: null };
+  // validate topics
+  if (!Array.isArray(data.topics) || data.topics.length < 1) {
+    return {
+      data: null,
+      error: new Error("Topics should be an array and should have at least one topic!")
     }
-    else {
+  }
+
+  const topics = await slugifyArray(data.topics);
+
+  // Check if the story is of kind === post
+  if (data.kind === 'post') {
+
+    const validatedData = {
+      content: await sanitizeUtil.sanitizeInput(data.content),
+      topics: topics,
+      kind: data.kind
+    }
+
+    return { data: validatedData, error: null };
+  }
+  else {
+    if (!data.title && !data.body && !data.slug) {
       return {
         data: null,
-        error: new Error("Some fields were not provided or contains null values, Ensure you provide: (title, body, topic)")
+        error: new Error("Missing fields, make sure you provide all the fields required!")
       }
     }
 
+    if (typeof data.title !== 'string' || data.title.length < 5) {
+      return {
+        data: null,
+        error: new Error("Title should be a string and should be at least 5 chars long!")
+      }
+    }
+
+    // validate last name
+    if (typeof data.body !== 'string' || data.body < 30) {
+      return {
+        data: null,
+        error: new Error("Body should be a string and should be at least 30 chars long!")
+      }
+    }
+
+    // validate last name
+    if (typeof data.slug !== 'string' || data.slug < 6) {
+      return {
+        data: null,
+        error: new Error("Slug should be a string and should be at least 6 chars long!!")
+      }
+    }
+
+    const slug_data = await slugify(data.slug);
+
+    const validatedData = {
+      title: await sanitizeUtil.sanitizeInput(data.title),
+      content: await sanitizeUtil.sanitizeInput(data.content),
+      body: await sanitizeUtil.sanitizeInput(data.body),
+      topics: topics,
+      kind: data.kind,
+      slug: slug_data
+    }
+
+    return { data: validatedData, error: null };
   }
 }
 
