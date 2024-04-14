8b983ee7dd0e41c5d85ee181043add067f2b0d7a
fescii <isfescii@gmail.com>
2024-04-14 13:50:44 +0300

    Story Entry: A27

--- src/validators/story.validator.js
+++ src/validators/story.validator.js
@@ -4,6 +4,8 @@ const { StoryType } = require('../configs').platformConfig;
 
 // New story data validation
 const validateStoryData = async (data) => {
+
+  // Check if all fields are provided
   if (!data.kind || !data.content || !data.topics) {
     return {
       data: null,
@@ -19,7 +21,7 @@ const validateStoryData = async (data) => {
     }
   }
 
-  // validate content
+  // Check if content is a string and is at least 2 chars long
   if (typeof data.content !== 'string' || data.content.length < 2) {
     return {
       data: null,
@@ -27,7 +29,7 @@ const validateStoryData = async (data) => {
     }
   }
 
-  // validate topics
+  // Check if topics is an array and has at least one topic
   if (!Array.isArray(data.topics) || data.topics.length < 1) {
     return {
       data: null,
@@ -35,22 +37,26 @@ const validateStoryData = async (data) => {
     }
   }
 
+  // Slugify topics
   const topics = slugifyArray(data.topics);
 
-  // console.log(topics);
 
   // Check if the story is of kind === post
   if (data.kind === 'post') {
 
+    // Create post data object
     const validatedData = {
       content: await sanitizeUtil.sanitizeInput(data.content),
       topics: topics,
       kind: data.kind
     }
 
+    // Return validated data
     return { data: validatedData, error: null };
   }
   else {
+
+    // Check if all fields for story are provided
     if (!data.title && !data.body && !data.slug) {
       return {
         data: null,
@@ -58,6 +64,7 @@ const validateStoryData = async (data) => {
       }
     }
 
+    // Check if title is a string and is at least 5 chars long
     if (typeof data.title !== 'string' || data.title.length < 5) {
       return {
         data: null,
@@ -65,7 +72,7 @@ const validateStoryData = async (data) => {
       }
     }
 
-    // validate last name
+    // check if body is a string and is at least 30 chars long
     if (typeof data.body !== 'string' || data.body < 30) {
       return {
         data: null,
@@ -73,16 +80,18 @@ const validateStoryData = async (data) => {
       }
     }
 
-    // validate last name
-    if (typeof data.slug !== 'string' || data.slug < 6) {
+    // check if slug is a string and is at least 5 chars long
+    if (typeof data.slug !== 'string' || data.slug < 5) {
       return {
         data: null,
-        error: new Error("Slug should be a string and should be at least 6 chars long!!")
+        error: new Error("Slug should be a string and should be at least 5 chars long!!")
       }
     }
 
+    // Slugify the slug
     const slug_data = slugify(data.slug);
 
+    // Create story data object
     const validatedData = {
       title: await sanitizeUtil.sanitizeInput(data.title),
       content: await sanitizeUtil.sanitizeInput(data.content),
@@ -92,12 +101,23 @@ const validateStoryData = async (data) => {
       slug: slug_data
     }
 
+    // Return validated data
     return { data: validatedData, error: null };
   }
 }
 
 // Story content validation
 const validateStoryContent = async (data) => {
+
+  // Check if content field is provided
+  if(!data.content) {
+    return {
+      data: null,
+      error: new Error("Missing fields, make sure you provide all the fields required!")
+    }
+  }
+
+  // Check if content is a string and is at least 3 chars long
   if (typeof data.content !== 'string' || data.content.length < 3) {
     return {
       data: null,
@@ -105,12 +125,60 @@ const validateStoryContent = async (data) => {
     };
   }
 
+  // Return data if all fields are valid
   return {
     data: await sanitizeUtil.sanitizeInput(data.content),
     error: null
   };
 }
 
+// Story body validation
+const validateStoryBody = async (data) => {
+
+  // Check if body field is provided
+  if (!data.kind || !data.body ) {
+    return {
+      data: null,
+      error: new Error("Missing fields, make sure you provide all the fields required!")
+    }
+  }
+
+  // Check if story kind is valid
+  if (!StoryType.includes(data.kind)) {
+    return {
+      data: null,
+      error: new Error("Invalid story kind, make sure you provide a valid story kind!")
+    }
+  }
+
+  // Check if story kind is neither 'post' nor 'poll'
+  if (data.kind !== 'post' && data.kind !== 'poll') {
+    return {
+      data: null,
+      error: new Error("Invalid story kind, make sure you provide a valid story kind!")
+    }
+  }
+
+  // Check if body is a string and is at least 30 chars long
+  if (typeof data.body !== 'string' || data.body.length < 30) {
+    return {
+      data: null,
+      error: new Error("Body should be a string and should be at least 30 chars long!")
+    };
+  }
+
+  // Return data if all fields are valid
+  return {
+    data: {
+      kind: data.kind,
+      body: await sanitizeUtil.sanitizeInput(data.body)
+    },
+    error: null
+  };
+}
+
+
+// Export the module
 module.exports = {
-  validateStoryData, validateStoryContent
+  validateStoryData, validateStoryContent, validateStoryBody
 }
