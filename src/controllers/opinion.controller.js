d1890a3d08181911638d05e3bf375f6b879d81e5
fescii <isfescii@gmail.com>
2024-04-15 19:09:19 +0300

    Story Entry: A39

---
+++ src/controllers/opinion.controller.js
@@ -0,0 +1,206 @@
+// Importing from internal modules
+const { checkAuthority } = require('../utils').roleUtil;
+const {
+  addOpinion, editOpinion, removeOpinion, addParentOpinion
+} = require('../queries').opinionQueries;
+
+const { validateOpinionData } = require('../validators').opinionValidator;
+
+
+// Create new opinion
+const createOpinion = async (req, res, next) => {
+  // Check if the payload or params is valid
+  if (!req.body || !req.user || !req.params) {
+    const error = new Error('Payload data or user data is undefined!');
+    return next(error)
+  }
+
+  // Get story hash from params
+  const storyHash = req.params.storyHash;
+
+  // Get user id from user data
+  const userId = req.user.id;
+
+  // Get opinion data from payload
+  const data = req.body;
+
+  // Validate the opinion data
+  const valObj = await validateOpinionData(data);
+
+  // If validation failed, return the error
+  if (valObj.error) {
+    return res.status(400).json({
+      success: false,
+      message: valObj.error.message
+    })
+  }
+
+  // Add the opinion
+  const {
+    opinion,
+    error
+  } = await addOpinion(userId, storyHash, valObj.data);
+
+  // If error occurred, return the error
+  if (error) {
+    return next(error);
+  }
+
+  // Return the success message
+  return res.status(201).json({
+    success: true,
+    message: 'Your opinion was added successfully!',
+    opinion
+  })
+}
+
+
+// Add Parent Opinion
+const createParentOpinion = async (req, res, next) => {
+  // Check if the payload or params is valid
+  if (!req.body || !req.user || !req.params) {
+    const error = new Error('Payload data or user data is undefined!');
+    return next(error)
+  }
+
+  // Get opinion hash from params
+  const opinionHash = req.params.opinionHash;
+
+  // Get user id from user data
+  const userId = req.user.id;
+
+  // Get opinion data from payload
+  const data = req.body;
+
+  // Validate the opinion data
+  const valObj = await validateOpinionData(data);
+
+  // If validation failed, return the error
+  if (valObj.error) {
+    return res.status(400).json({
+      success: false,
+      message: valObj.error.message
+    })
+  }
+
+  // Add the opinion
+  const {
+    opinion,
+    error
+  } = await addParentOpinion(userId, opinionHash, valObj.data);
+
+  // If error occurred, return the error
+  if (error) {
+    return next(error);
+  }
+
+  // Return the success message
+  return res.status(201).json({
+    success: true,
+    message: 'Your opinion was added successfully!',
+    opinion
+  })
+}
+
+// Edit opinion
+const updateOpinion = async (req, res, next) => {
+  // Check if the payload or params is valid
+  if (!req.body || !req.user || !req.params) {
+    const error = new Error('Payload data or user data is undefined!');
+    return next(error)
+  }
+
+  // Get opinion hash from params
+  const opinionHash = req.params.opinionHash;
+
+  // Get user id from user data
+  const userId = req.user.id;
+
+  // Get opinion data from payload
+  const data = req.body;
+
+  // Validate the opinion data
+  const valObj = await validateOpinionData(data);
+
+  // If validation failed, return the error
+  if (valObj.error) {
+    return res.status(400).json({
+      success: false,
+      message: valObj.error.message
+    })
+  }
+
+  // Edit the opinion
+  const {
+    opinion,
+    error
+  } = await editOpinion(userId, opinionHash, valObj.data);
+
+  // If error occurred, return the error
+  if (error) {
+    return next(error);
+  }
+
+  // Return the success message
+  return res.status(200).json({
+    success: true,
+    message: 'Your opinion was edited successfully!',
+    opinion
+  })
+}
+
+// Controller for deleting/removing opinion
+const deleteOpinion = async (req, res, next) => {
+  // Check if the payload or params is valid
+  if (!req.params || !req.user) {
+    const error = new Error('Payload data or user data is undefined!');
+    return next(error)
+  }
+
+  // Get opinion hash from params
+  const opinionHash = req.params.opinionHash;
+
+  // Get user id from user data
+  const userId = req.user.id;
+
+  // Delete the opinion
+  const {
+    opinion,
+    author,
+    error
+  } = await removeOpinion(userId, opinionHash);
+
+  // If error occurred, return the error
+  if (error) {
+    return next(error);
+  }
+
+  // Check if there no opinion was found
+  if (!opinion) {
+    return res.status(404).json({
+      success: false,
+      message: 'The opinion you are trying to delete was not found!'
+    })
+  }
+
+  // Check of author is was false (meaning the user isn't the author)
+  if (!author) {
+    return res.status(403).json({
+      success: false,
+      message: 'You are not authorized to delete this opinion!'
+    })
+  }
+
+  // Return the success message
+  return res.status(200).json({
+    success: true,
+    message: 'Your opinion was deleted successfully!'
+  })
+}
+
+// Export all controllers
+module.exports = {
+  createOpinion,
+  updateOpinion,
+  createParentOpinion
+}
