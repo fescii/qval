ee48c89e308844763f3bd369fcbac9dfb72c18a1
fescii <isfescii@gmail.com>
2024-06-16 20:50:26 +0300

    Topic: Entry A35

--- src/validators/users/base.user.js
+++ src/validators/users/base.user.js
@@ -1,13 +1,13 @@
-const { sanitizeUtil } = require('../../utils')
+const { sanitizeUtil } = require('../../utils');
 
 /**
- * @function validateUserData
- * @name validateUserData
+ * @function validateUser
+ * @name validateUser
  * @description A validator function to validate user data before being passed controllers or middlewares
  * @param {Object} data - The input data from client request
  * @returns {Object} - The validated topic data object
 */
-const validateUserData = async (data) => {
+const validateUser = async data => {
   if (data.first_name && data.last_name && data.email && data.password) {
     // validate first name
     if (typeof data.first_name !== 'string' || data.first_name.length < 2) {
@@ -62,13 +62,13 @@ const validateUserData = async (data) => {
 }
 
 /**
- * @function validateLoginData
- * @name validateLoginData
+ * @function validateLogin
+ * @name validateLogin
  * @description A validator function to validate user data before being passed controllers or middlewares
  * @param {Object} data - The input data from client request
  * @returns {Object} - The validated topic data object
 */
-const validateLoginData = async (data) => {
+const validateLogin = async data => {
   if (data.email && data.password) {
 
     // validate username
@@ -114,168 +114,10 @@ const validateLoginData = async (data) => {
 }
 
 /**
- * @function validatePassword
- * @name validatePassword
- * @description A validator function to validate password before being passed controllers or middlewares
- * @param {Object} data - The input data from client request
- * @returns {Object} - The validated password object
- * @returns {Error} - The error object
-*/
-const validatePassword = async (data) => {
-  //check if password is provided
-  if (!data.password) {
-    return {
-      data: null,
-      error: new Error("Password field is required!")
-    }
-  }
-  if (typeof data.password !== 'string' || data.password.length < 6) {
-    return {
-      data: null,
-      error: new Error("Password should have 6 chars or more and must be a string!")
-    }
-  }
-  return {
-    data: {
-      password: data.password
-    },
-    error: null
-  }
-}
-
-/**
- * @function validateUsername
- * @name validateUsername
- * @description A validator function to validate username before being passed controllers or middlewares
- * @param {Object} data - The input data from client request
- * @returns {Object} - The validated username object
- * @returns {Error} - The error object
-*/
-const validateEmail = async (data) => {
-  //check if email is provided
-  if (!data.email) {
-    return {
-      data: null,
-      error: new Error("Email field is required!")
-    }
-  }
-
-  //sanitize email
-  const email = await sanitizeUtil.sanitizeInput(data.email)
-
-  // validate email
-  // noinspection RegExpRedundantEscape
-  let validRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-  if (!email.match(validRegex)){
-    return {
-      data: null,
-      error: new Error("Invalid email address!")
-    }
-  }
-
-  return {
-    data: {
-      email: email
-    },
-    error: null
-  }
-}
-
-/**
- * @function validateUsername
- * @name validateUsername
- * @description A validator function to validate username before being passed controllers or middlewares
- * @param {Object} data - The input data from client request
- * @returns {Object} - The validated username object
- * @returns {Error} - The error object
-*/
-const validateContact = async (data) => {
-  //check if contact is provided
-  if (!data.contact) {
-    return {
-      data: null,
-      error: new Error("Contact field is required!")
-    }
-  }
-
-  // Check if contact is type of object/json
-  if (typeof data.contact !== 'object') {
-    return {
-      data: null,
-      error: new TypeError("Contact must be an object!")
-    }
-  }
-
-  return {
-    data: {
-      contact: data.contact
-    },
-    error: null
-  }
-}
-
-/**
- * @function validateBio
- * @name validateBio
- * @description A validator function to validate bio before being passed controllers or middlewares
- * @param {Object} data - The input data from client request
- * @returns {Object} - The validated bio object
- * @returns {Error} - The error object
-*/
-const validateBio = async (data) => {
-  //check if bio is provided
-  if (!data.bio) {
-    return {
-      data: null,
-      error: new Error("Bio field is required!")
-    }
-  }
-
-  //sanitize bio
-  const bio = await sanitizeUtil.sanitizeInput(data.bio)
-
-  return {
-    data: { bio: bio },
-    error: null
-  }
-}
-
-/**
- * @function validateName
- * @name validateName
- * @description A validator function to validate name before being passed controllers or middlewares
- * @param {Object} data - The input data from client request
- * @returns {Object} - The validated name object or an error object
-*/
-const validateName = async (data) => {
-  //check if name is provided
-  if (!data.first_name || !data.last_name) {
-    return {
-      data: null,
-      error: new Error("First name and Last name fields are required!")
-    }
-  }
-
-  //sanitize name
-  const name = `${await sanitizeUtil.sanitizeInput(data.first_name)} ${await sanitizeUtil.sanitizeInput(data.last_name)}`
-
-  return {
-    data: { name: name },
-    error: null
-  }
-}
-
-/**
  * @module Export all validators
  * @name Validators
  * @description Export all validators as an object
 */
 module.exports = {
-  validateUserData,
-  validateLoginData,
-  validatePassword,
-  validateEmail,
-  validateContact,
-  validateBio, validateName
+  validateUser, validateLogin
 }
