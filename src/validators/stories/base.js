82db2f9b97fcd12c125ddaa3d972d37337f7b01b
fescii <isfescii@gmail.com>
2024-06-24 10:43:30 +0300

    Modified: Story model - added tsvector for full_text search; Modified Reply, Like, View, and Topic Models - added hooks and queues and altered associations

---
+++ src/validators/stories/base.js
@@ -0,0 +1,13 @@
+// Import sanitizeUtil from src/utils/sanitize.util.js
+const sanitizeUtil = require('../../utils/sanitizer.util');
+
+/**
+ * @name validateStory
+ * @function validateStory
+ * @description a validator function that validates story data before being passed to the controllers or middlewares
+ * @param {Object} data - The story data object
+ * @returns {Object} data - The validated story data object and error if any
+*/
+const validateStory = async data => {
+  
+}
